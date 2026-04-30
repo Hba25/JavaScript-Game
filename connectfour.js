b74a0b108f95780285d5aed982a5008a2dd2b3c9
‘Hba25’ <bh25dev@outlook.com>
2026-04-30 13:32:37 +0100

    Vamped JS logic, event added to clicking a column, logic for empty slots and if a token can be placed there and allows the current player to, logic for which players turn it is and switch each turn, added class dynamically for each players token, linked the slot div to the js logic so it uses data-row and data-column

--- connectfour.js
+++ connectfour.js
@@ -7,4 +7,34 @@ for (let x = 0; x <= 5; x++) {
   }
   gameBoard.push(row);
 }
-console.log(gameBoard);
+
+const colTrigger = document.querySelectorAll(".column");
+
+let currentPlayer = "player1";
+colTrigger.forEach((column, index) =>
+  column.addEventListener("click", function (e) {
+    for (let row = 5; row >= 0; row--) {
+      if (gameBoard[row][index] === null) {
+        console.log(row, index);
+        gameBoard[row][index] = currentPlayer;
+        console.log(gameBoard);
+        const colourObj = document.querySelector(
+          `.slot[data-row= "${row}"][data-column= "${index}"]`,
+        );
+        const placingPlayer = currentPlayer;
+        if (placingPlayer === "player1") {
+          colourObj.classList.add("redToken");
+        } else {
+          colourObj.classList.add("goldToken");
+        }
+
+        if (currentPlayer === "player1") {
+          currentPlayer = "player2";
+        } else {
+          currentPlayer = "player1";
+        }
+        break;
+      }
+    }
+  }),
+);
